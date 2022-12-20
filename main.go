ede0869a06e039fd38b79b1449ab4ce3a690200e
zeimedee <42755623+zeimedee@users.noreply.github.com>
2022-12-20 22:22:28 +0000

    slightly techie deploy on redner

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"log"
+	// "log"
 	"os"
 
 	"github.com/zeimedee/slightlytechie/router"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading env file")
+	// }
 
 	port := os.Getenv("PORT")
 
